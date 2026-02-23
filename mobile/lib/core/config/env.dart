8a0c3633d91f8ca8ea451bd899543f26bd092760
Prasad <prasads6507@gmail.com>
2026-02-22 20:42:05 -0500

    fix: re-applied firebase rules and network config to version eebf926

--- mobile/lib/core/config/env.dart
+++ mobile/lib/core/config/env.dart
@@ -1,4 +1,4 @@
 class Env {
-  static const String apiUrl = 'http://192.168.68.59:3001';
+  static const String apiUrl = 'http://192.168.68.51:3001';
   static const bool isProduction = bool.fromEnvironment('dart.vm.product');
 }
