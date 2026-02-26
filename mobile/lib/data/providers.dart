ff855a7ef6b6ab3e71195be6803bd9cdaa17be48
Prasad <prasads6507@gmail.com>
2026-02-25 22:58:24 -0500

    Version 0.6.0 Push Notification Successfull sending to the students

--- mobile/lib/data/providers.dart
+++ mobile/lib/data/providers.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
