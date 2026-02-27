efc7dbe0b1224eac6bc837b5a9acfcd4e5045861
kk-ry <sivasaikumar.komma@uncode.io>
2026-02-26 23:03:18 -0500

    Fix undefined name Env in providers.dart

--- mobile/lib/data/providers.dart
+++ mobile/lib/data/providers.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../core/config/env.dart';
 
 import 'datasources/api_ds.dart';
 import 'datasources/auth_ds.dart';
